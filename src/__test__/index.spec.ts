3f91546d2a19d69f4c2860db419baed10e4f60ed
boyone <boyone.g@gmail.com>
2020-02-12 10:23:27 +0700

    Reformat file.

--- src/__test__/index.spec.ts
+++ src/__test__/index.spec.ts
@@ -1,9 +1,10 @@
-import hello from '..'
-describe("Hello", () => {
-    it("empty hello", () => {
-      expect(hello()).toBe("Hello, World");
-    });
-    it("hello world", () => {
-      expect(hello('World')).toBe("Hello, World");
-    });
+import hello from '..';
+
+describe('Hello', () => {
+  it('empty hello', () => {
+    expect(hello()).toBe('Hello, World');
   });
+  it('hello world', () => {
+    expect(hello('World')).toBe('Hello, World');
+  });
+});
