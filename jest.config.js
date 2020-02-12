4b7f91be0656e5ba38d6d3d03d6d7d1aea32bc71
boyone <boyone.g@gmail.com>
2020-02-12 10:24:59 +0700

    Add new config for jest-junit.

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,23 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   preset: 'ts-jest',
-  reporters: ['default', ['jest-junit', { suiteName: 'jest tests' }]]
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+  reporters: ['default', ['jest-junit', { suiteName: 'jest tests' }]],
+  globals: {
+    'ts-jest': {
+      tsConfig: {
+        importHelpers: true
+      }
+    }
+  },
+  "roots": [
+    "<rootDir>/src"
+  ],
+  "transform": {
+    "^.+\\.ts$": "ts-jest"
+  },
+  "moduleFileExtensions": [
+    "ts",
+    "js"
+  ]
 };
